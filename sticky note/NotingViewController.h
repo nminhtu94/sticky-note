71164d288c83fa89486fb38b3bee23f509393ec8
Le Thanh Tan <thanhtan1105@gmail.com>
2015-08-02 19:50:23 +0700

    [Tan] commit

---
+++ sticky note/NotingViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotingViewController.h
+//  sticky note
+//
+//  Created by Le Thanh Tan on 8/2/15.
+//  Copyright (c) 2015 Apps Fellow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotingViewController : UIViewController
+
+@end
---
+++ sticky note/NotingViewController.m
@@ -0,0 +1,37 @@
+//
+//  NotingViewController.m
+//  sticky note
+//
+//  Created by Le Thanh Tan on 8/2/15.
+//  Copyright (c) 2015 Apps Fellow. All rights reserved.
+//
+
+#import "NotingViewController.h"
+
+@interface NotingViewController ()
+
+@end
+
+@implementation NotingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
