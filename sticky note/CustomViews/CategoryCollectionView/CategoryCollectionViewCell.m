b941893ecc839ab81e9048d43638801ba408851c
nminhtu94 <nminhtu94@gmail.com>
2015-09-17 12:24:01 +0700

    corner

--- sticky note/CustomViews/CategoryCollectionView/CategoryCollectionViewCell.m
+++ sticky note/CustomViews/CategoryCollectionView/CategoryCollectionViewCell.m
@@ -1,17 +1,10 @@
-//
-//  CategoryCollectionViewCell.m
-//  sticky note
-//
-//  Created by Nguyen Minh Tu on 5/31/15.
-//  Copyright (c) 2015 Apps Fellow. All rights reserved.
-//
-
 #import "CategoryCollectionViewCell.h"
 
 @implementation CategoryCollectionViewCell
 
 - (void)awakeFromNib {
-    // Initialization code
+  [self.imgIcon.layer setCornerRadius:5.0f];
+  [self.imgIcon setClipsToBounds:YES];
 }
 
 @end
